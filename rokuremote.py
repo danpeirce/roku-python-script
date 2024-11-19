450ef9b374e9baf635d2dce5b6cac1acd31cf373
Dan Peirce <peirce.dan@gmail.com>
2024-11-19 15:30:19 -0800

    first commit

---
+++ rokuremote.py
@@ -0,0 +1,19 @@
+import http.client
+import urllib.request, urllib.parse, urllib.error
+
+def ecp_invoke(op, path):
+    conn = http.client.HTTPConnection(ROKU_IP, 8060)
+    conn.request(op, path)
+    r = conn.getresponse()
+    res = r.read() if r.status==200 else '%d[%s]=' % (r.status, r.reason)
+    conn.close()
+    return res
+
+ROKU_IP = '192.168.0.25'
+get_app_list = lambda: ecp_invoke('GET', '/query/apps')
+keypress = lambda key: ecp_invoke('POST', '/keypress/' + key)
+keydown = lambda key: ecp_invoke('POST', '/keydown/' + key)
+keyup = lambda key: ecp_invoke('POST', '/keyup/' + key)
+launch = lambda appID: ecp_invoke('POST', '/launch/' + appID)
+send_input = lambda dict: ecp_invoke('POST', '/input?' + '&'.join(map('='.join, list(dict.items()))))
+
